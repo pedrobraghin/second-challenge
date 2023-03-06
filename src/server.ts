c10a6238017acc5239da833ad1a57e4b38c5edac
Pedro Braghin <braghindev@gmail.com>
2023-03-06 17:09:42 -0300

    refactor: move database connection to'db' file

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,12 @@
 import 'dotenv/config';
-import mongoose from 'mongoose';
+import { connect } from './database/db';
 
 import { app } from './app';
-
-const DB = process.env.DATABASE!.replace(
-	'<PASSWORD>',
-	process.env.DATABASE_PASSWORD!
-);
-
-mongoose.connect(DB).then((con) => {
-	console.log('Connected to database!');
-});
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, async () => {
+	await connect();
 	console.log(
 		`Listening to port ${port}, environment: ${process.env.NODE_ENV}`
 	);
