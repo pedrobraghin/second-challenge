8352379e167877aed813640e89a1357d5a69165c
Pedro <pedroldacs@gmail.com>
2023-03-08 11:08:56 -0400

    feat: add body parser middleware on app.ts
    Co-authored-by: Pedro Braghin <pedrobraghin@users.noreply.github.com>
    Co-authored-by: Fernanda Pessoa <fernandapessoa@users.noreply.github.com>

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { errorHandler, notFoundRouteHandler } from './routes/globals.routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/v1', router);
 
 app.all('*', notFoundRouteHandler);
