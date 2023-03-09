2d412b669d4abbb54b2b493cd0bcad1b54fb8b78
Pedro <pedroldacs@gmail.com>
2023-03-08 22:22:49 -0400

    feat: implement getEventByWeekday on repository

--- src/repositories/implementations/MongoEventsRepository.ts
+++ src/repositories/implementations/MongoEventsRepository.ts
@@ -8,21 +8,38 @@ export class MongoEventsRepository implements IEventsRepository {
 		const createdEvent = await EventModel.create(event);
 		return createdEvent;
 	}
+
 	async getAllEvents(): Promise<Event[]> {
 		const events = await EventModel.find();
 		return events;
 	}
+
 	async getEventById(eventId: string): Promise<Event | null> {
 		const event = await EventModel.findById(eventId);
 		return event;
 	}
-	async getEventByWeekDay(weekDay: string): Promise<Event | null> {
-		throw new Error('Method not implemented.');
+
+	async getEventByWeekDay(weekDay: string): Promise<Event[]> {
+		const events = await EventModel.find();
+
+		const filtered: Event[] = events.filter((event) => {
+			const eventWeekDay = new Date(event.dateTime)
+				.toLocaleString('en-us', {
+					weekday: 'long',
+				})
+				.toLowerCase();
+
+			return weekDay === eventWeekDay;
+		});
+
+		return filtered;
 	}
+
 	async deleteEventById(eventId: string): Promise<Event | null> {
 		const deletedEvent = await EventModel.findByIdAndDelete(eventId);
 		return deletedEvent;
 	}
+
 	async deleteEventByWeekDay(weekDay: string): Promise<Event | null> {
 		throw new Error('Method not implemented.');
 	}
