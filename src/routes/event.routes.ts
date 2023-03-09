034145790b6375284bb123be0ed94887fa80da84
Pedro Braghin <braghindev@gmail.com>
2023-03-09 09:34:47 -0300

    feat: add auth middleware to create event route

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
 import { createEventController } from '../use-cases/event';
+import { auth } from '../middlewares/auth';
 
 const eventsRouter = Router({ mergeParams: true });
 
-eventsRouter.post('/', createEventController.handle);
+eventsRouter.post('/', auth, createEventController.handle);
 
 export { eventsRouter };
