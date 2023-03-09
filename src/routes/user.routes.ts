febe20d7a0572cfccdf459cb40c83df28bb57a52
Pedro <pedroldacs@gmail.com>
2023-03-09 14:54:02 -0400

    feat: define user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
-import { createUserController, loginUserController } from '../use-cases/user';
+import {
+	createUserController,
+	loginUserController,
+	getUserByIdController,
+	updateUserByIdController,
+	deleteUserByIdController,
+} from '../use-cases/user';
+
+import { auth } from '../middlewares/auth';
 
 const usersRouter = Router();
 
 usersRouter.post('/signUp', createUserController.handle);
 usersRouter.post('/signIn', loginUserController.handle);
+
+usersRouter.get('/me', auth, getUserByIdController.handle);
+usersRouter.patch('/me', auth, updateUserByIdController.handle);
+usersRouter.delete('/me', auth, deleteUserByIdController.handle);
 
 export { usersRouter };
