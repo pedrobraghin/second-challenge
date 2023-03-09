5e3c925908bca321fa65318cc0fceec0330996a6
Pedro Braghin <braghindev@gmail.com>
2023-03-09 09:09:52 -0300

    refactor: improve error handling to auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,13 +6,21 @@ import { getUserByIdService } from '../use-cases/user/getUser/GetUserFactory';
 export async function auth(req: Request, res: Response, next: NextFunction) {
 	const token = req.headers.authorization?.split(' ')[1];
 
-	if (!token) throw new AppError(401, 'Invalid or expired token');
+	if (!token) {
+		return next(new AppError(401, 'Invalid or expired token'));
+	}
 
 	const payload = verifyToken(token);
 
-	if (!payload) throw new AppError(401, 'Invalid or expired token');
+	if (!payload) {
+		return next(new AppError(401, 'Invalid or expired token'));
+	}
 
-	req.body.user = await getUserByIdService.execute(payload.id);
+	try {
+		req.body.user = await getUserByIdService.execute(payload.id);
+	} catch (err) {
+		return next(new AppError(401, 'Invalid or expired token'));
+	}
 
 	next();
 }
