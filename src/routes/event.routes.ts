84455b3207945ec2e78c4ab612d9827f19f85be0
Pedro Braghin <braghindev@gmail.com>
2023-03-09 09:34:06 -0300

    refactor: improve route handling

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,32 +1,9 @@
 import { Router } from 'express';
-// import { AppError } from '../errors/AppError';
 
-import {
-	createEventController,
-	// getAllEventsController,
-	// deleteEventByIdController,
-	// deleteEventByWeekDayController,
-	// getEventById,
-} from '../use-cases/event';
+import { createEventController } from '../use-cases/event';
 
 const eventsRouter = Router({ mergeParams: true });
 
-eventsRouter.post('/', (req, res, next) => {
-	console.log(createEventController);
-	createEventController.handle(req, res, next);
-});
-
-// eventsRouter.get('/:id', getEventById.handle);
-// eventsRouter.get('/', getAllEventsController.handle);
-
-// eventsRouter.delete('/:id', deleteEventByIdController.handle);
-// eventsRouter.delete('/', (req, res, next) => {
-// 	const { weekDay } = req.query;
-
-// 	if (!weekDay) {
-// 		return next(new AppError(400, 'A week day is required'));
-// 	}
-// 	deleteEventByWeekDayController.handle(req, res, next);
-// });
+eventsRouter.post('/', createEventController.handle);
 
 export { eventsRouter };
