a5b4555c655b22c7abede9300cb30d55aaf4fe94
Pedro Braghin <braghindev@gmail.com>
2023-03-09 11:01:57 -0300

    feat: add user schema validator middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { createUserController, loginUserController } from '../use-cases/user';
-
+import { validateUserData } from '../middlewares/valiteUserData';
 const usersRouter = Router();
 
-usersRouter.post('/signUp', createUserController.handle);
+usersRouter.post('/signUp', validateUserData, createUserController.handle);
 usersRouter.post('/signIn', loginUserController.handle);
 
 export { usersRouter };
