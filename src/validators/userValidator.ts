fbbffa690986c8b03f7a130edfc66a8987826a6d
Pedro Braghin <braghindev@gmail.com>
2023-03-09 10:59:59 -0300

    feat: implement user schema validator

---
+++ src/validators/userValidator.ts
@@ -0,0 +1,16 @@
+import Joi from 'joi';
+
+const userSchema = Joi.object({
+	firstName: Joi.string().alphanum().min(3).max(30).required(),
+	lastName: Joi.string().alphanum().min(3).max(30).required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().min(8).required(),
+	confirmPassword: Joi.ref('password'),
+	city: Joi.string().alphanum().required(),
+	country: Joi.string().alphanum().required(),
+	birthDate: Joi.date()
+		.min(new Date().getFullYear() - 130)
+		.max(new Date().getFullYear() - 6),
+});
+
+export { userSchema };
