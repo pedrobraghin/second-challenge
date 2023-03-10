04e305ce2792a5d8f93f1549a5671226893425a0
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-03-09 21:55:39 -0400

    feat: add eventsRepository in DeleteUserByIdService

--- src/use-cases/user/deleteUser/DeleteUserByIdService.ts
+++ src/use-cases/user/deleteUser/DeleteUserByIdService.ts
@@ -1,17 +1,23 @@
 import { AppError } from '../../../errors/AppError';
 import { IUsersRepository } from '../../../repositories/IUsersRepository';
+import { IEventsRepository } from '../../../repositories/IEventsRepository';
 
 export class DeleteUserByIdService {
 	private usersRepository: IUsersRepository;
+	private eventsRepository: IEventsRepository;
 
-	constructor(usersRepository: IUsersRepository) {
+	constructor(
+		usersRepository: IUsersRepository,
+		eventsRepository: IEventsRepository
+	) {
 		this.usersRepository = usersRepository;
+		this.eventsRepository = eventsRepository;
 	}
 
 	async execute(id: string) {
 		const user = await this.usersRepository.updateUser(id, { active: false });
-
 		if (!user) throw new AppError(404, 'User not found');
+		await this.eventsRepository.deleteAllUserEvents(id);
 
 		return user;
 	}
