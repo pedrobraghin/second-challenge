0c81b699e81a697999ef17c9214a45eb1b30889e
Pedro Braghin <braghindev@gmail.com>
2023-03-13 14:05:42 -0300

    feat: add security middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import { router } from './routes/app.routes';
 import { errorHandler, notFoundRouteHandler } from './routes/globals.routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger/swagger.json';
+import cors from 'cors';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 const app = express();
 
+app.use(cors({ origin: '*' }));
+app.use(helmet());
 app.use(express.json());
+app.use(mongoSanitize());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', router);
