c88b35a2d74d91b006f989449d38eafbe7a7b9fc
Pedro Braghin <braghindev@gmail.com>
2023-03-13 10:22:54 -0300

    feat: add verifying if user password was updated

--- src/use-cases/user/updateUser/UpdateUserByIdService.ts
+++ src/use-cases/user/updateUser/UpdateUserByIdService.ts
@@ -1,5 +1,7 @@
 import { AppError } from '../../../errors/AppError';
 import { IUsersRepository } from '../../../repositories/IUsersRepository';
+import { hashPass } from '../../../security/security';
+import { User } from '../../../types/User';
 
 export class UpdateUserByIdService {
 	private usersRepository: IUsersRepository;
@@ -8,7 +10,10 @@ export class UpdateUserByIdService {
 		this.usersRepository = usersRepository;
 	}
 
-	async execute(id: string, updateParams: any) {
+	async execute(id: string, updateParams: Partial<User>) {
+		if (updateParams.password) {
+			updateParams.password = await hashPass(updateParams.password);
+		}
 		const user = await this.usersRepository.updateUser(id, updateParams);
 
 		if (!user) throw new AppError(404, 'User not found');
