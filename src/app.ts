769658592dadaf5116732fe393e28455cd7adfa2
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-03-12 20:56:47 -0400

    feat: swagger route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { router } from './routes/app.routes';
 import { errorHandler, notFoundRouteHandler } from './routes/globals.routes';
-
+import swaggerUi from 'swagger-ui-express';
+import { openapiSpecification } from './swagger/index';
 const app = express();
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 app.use('/api/v1', router);
-
 app.all('*', notFoundRouteHandler);
 app.use(errorHandler);
 
